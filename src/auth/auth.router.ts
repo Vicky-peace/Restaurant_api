25b7b7ee79cfd70c30b4852e017e62aa1ffa98ec
Vicky-peace <victorpeace47@gmail.com>
2024-06-11 16:32:15 +0300

    solved bug on auth.router

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,13 +1,9 @@
 import { Hono } from 'hono';
 import { register, login } from './auth.controller';
-import { authenticateToken } from '../middlewares/auth.middleware';
+
 
 export const authRouter = new Hono();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 
-// Example of a protected route
-authRouter.get('/protected', authenticateToken, (c) => {
-  return c.json({ msg: 'This is a protected route' }, 200);
-});
